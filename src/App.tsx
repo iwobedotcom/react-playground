6bb76b0b1cbfc73d6de2b223afa3d5a95ec5a07c
Iwobe Aigboje <iwobe.aigboje@gmail.com>
2023-12-30 17:58:01 +0100

    Installed and updated to '@tanstack/react-query'

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import SuperHeroes from "./components/super-heroes";
 import Header from "./components/header";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import SuperHero from "./components/super-hero";
 import ParallelQuery from "./components/parallel-query";
 import Form from "./components/form";
@@ -22,7 +22,10 @@ function App() {
             <Route path="/form" element={<Form />} />
           </Routes>
         </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
       </QueryClientProvider>
     </>
   );
